9232cbe996f9ab1ab1aeeeb71fb2feddb3bb1f0b
daishuhua <daishuhua@139.com>
2017-02-02 23:57:53 +0800

    init commit

---
+++ spring-di/src/main/java/com/daishuhua/App.java
@@ -0,0 +1,13 @@
+package com.daishuhua;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
